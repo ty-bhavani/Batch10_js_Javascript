c3486fa1ea43a79fd139cf5021beeffbe226429e
ty-bhavani <bhavaniyp02@gmail.com>
2021-05-14 11:02:45 +0530

    push

---
+++ ArrayDemo.js
@@ -0,0 +1,29 @@
+console.log("Array Properties");
+
+var brand=["nike","americaeagle","tommyhilfiger","bluetree","puma","rio"];
+
+
+// console.log("push operation")
+ //console.log(  brand.push("biba"));
+
+// console.log("pop operation");
+// console.log(brand.pop());  
+
+
+// console.log("Boolean includes operation");
+console.log(brand.includes("teamspirit",4));  
+//  console.log( brand.includes("nike",0));
+
+// console.log("array splice");
+//  console.log(brand.splice(1,2));   
+
+// console.log("array slice operation");
+// console.log(brand.slice(3,4)); 
+
+// console.log("array join");
+//  console.log(brand.join("hlo"));  
+
+// console.log("number indexof");
+// console.log(brand.indexOf("rio",0))
+
+//console.log(brand);
